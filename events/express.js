42a49623a08ba3e5ffa6e4e18b29a574d5e97fc0
tylerdotjs <tylerdotjs@gmail.com>
2020-12-09 23:45:15 -0600

    usernames of attending users are now sent with events request

--- events/express.js
+++ events/express.js
@@ -1,34 +1,55 @@
 const express = require('express');
 const passport = require('passport');
 const dbModel = require('./dbModel')
+const userModel = require("../user/dbModel")
 
 const app = express.Router()
 
 app.use(passport.authenticate('session'))
 
 app.get('/', (req, res) => {
-    dbModel.find((err, result) => {
-        if (err) throw err;
-        if (!req.user) return res.sendStatus(401)
-        var data = {
-            isAdmin: false,
-            events: []
-        }
-        if (req.user.roles.includes("admin")) {
-            data.isAdmin = true;
-        }
-        for(var i = 0; i < result.length; i++){
-            var e = result[i]
-            data.events[i] = {
-                _id: e._id,
-                subject: e.subject,
-                description: e.description,
-                date: e.date,
-                attending: e.attendees.includes(req.user._id)
+    dbModel.find()
+        .sort('-date')
+        .exec((err, result) => {
+            if (err) throw err;
+            if (!req.user) return res.sendStatus(401)
+            var data = {
+                isAdmin: false,
+                events: []
             }
-        }
-        res.send(data)
-    })
+            if (req.user.roles.includes("admin")) {
+                data.isAdmin = true;
+            }
+            var complatedQuerys = 0
+            result.forEach((e, index) => {
+                userModel.find({
+                    '_id': { $in: e.attendees }
+                })
+                    .exec((err, users) => {
+                        if (err) throw err;
+                        var usersClean = []
+                        users.forEach((el, userI) => {
+                            usersClean[userI] = {
+                                _id: el._id,
+                                username: el.username
+                            }
+                        })
+                        data.events[index] = {
+                            _id: e._id,
+                            subject: e.subject,
+                            description: e.description,
+                            date: e.date,
+                            attending: e.attendees.includes(req.user._id),
+                            peers: usersClean
+                            
+                        }
+                        complatedQuerys++
+                        if (complatedQuerys >= result.length) {
+                            return res.send(data)
+                        }
+                    })
+            })
+        })
 })
 app.post('/add', passport.useRole("admin"), (req, res) => {
     dbModel.create({ subject: req.body.subject, description: req.body.description, date: req.body.date, location: req.body.location }, (err) => {
@@ -37,17 +58,17 @@ app.post('/add', passport.useRole("admin"), (req, res) => {
     })
 })
 app.post('/join', (req, res) => {
-    if(!req.body.id) return res.sendStatus(403)
-    if(!req.user) return res.sendStatus(401)
-    dbModel.updateOne({_id: req.body.id}, {$push: {attendees: req.user._id}}, (err, response) => {
+    if (!req.body.id) return res.sendStatus(403)
+    if (!req.user) return res.sendStatus(401)
+    dbModel.updateOne({ _id: req.body.id }, { $push: { attendees: req.user._id } }, (err, response) => {
         if (err) throw err
         res.sendStatus(200)
     })
 })
 app.post('/cancel', (req, res) => {
-    if(!req.body.id) return res.sendStatus(403)
-    if(!req.user) return res.sendStatus(401)
-    dbModel.updateOne({_id: req.body.id}, {$pullAll: {attendees: [req.user._id]}}, (err, response) => {
+    if (!req.body.id) return res.sendStatus(403)
+    if (!req.user) return res.sendStatus(401)
+    dbModel.updateOne({ _id: req.body.id }, { $pullAll: { attendees: [req.user._id] } }, (err, response) => {
         if (err) throw err
         res.sendStatus(200)
     })
